7acf1a9f5a03a3d7508e18bc9f6e0084e84c8f95
Rob Gilbert <fox015@gmail.com>
2020-08-22 17:48:42 +1000

    Added debouncing.

--- main.ts
+++ main.ts
@@ -9,17 +9,10 @@ namespace keypad {
         ["A", "B", "C", "D"]
     ];
     let maxPins=3;
+    let debounceMs = 100
 
-    //% blockId=setKeyPad3 block="configure pins: row1 %row1|row2 %row2|row3 %row3|row4 %row4|col1 %col1|col2 %col2|col3 %col3" blockExternalInputs=false
-    //% weight=70
-    //% pin1.fieldEditor="gridpicker" pin1.fieldOptions.columns=4
-    //% pin2.fieldEditor="gridpicker" pin2.fieldOptions.columns=4
-    //% pin3.fieldEditor="gridpicker" pin3.fieldOptions.columns=4
-    //% pin4.fieldEditor="gridpicker" pin4.fieldOptions.columns=4
-    //% pin5.fieldEditor="gridpicker" pin5.fieldOptions.columns=4
-    //% pin6.fieldEditor="gridpicker" pin6.fieldOptions.columns=4
-    //% pin7.fieldEditor="gridpicker" pin7.fieldOptions.columns=4
-    export function setKeyPad3(row1: DigitalPin, row2: DigitalPin, row3: DigitalPin, row4: DigitalPin, col1: DigitalPin, col2: DigitalPin, col3: DigitalPin): void {
+    //% blockId=configurePins block="configure pins: row1 %row1|row2 %row2|row3 %row3|row4 %row4|col1 %col1|col2 %col2|col3 %col3" blockExternalInputs=false
+    export function configurePins(row1: DigitalPin, row2: DigitalPin, row3: DigitalPin, row4: DigitalPin, col1: DigitalPin, col2: DigitalPin, col3: DigitalPin): void {
         rows[0] = row1;
         rows[1] = row2;
         rows[2] = row3;
@@ -30,9 +23,18 @@ namespace keypad {
         maxPins=3;
     }
 
+    //% blockId=setDebounceMs block="set debounce (ms) %ms" blockExternalInputs=false
+    export function setDebounceMs(ms: number): void {
+        debounceMs = ms;
+    }
+
     //% blockId=getKeyString block="key"
     export function getKeyString(): string {
         let myString = "";
+        
+        // Debounce
+        basic.pause(debounceMs)
+        
         // Scan through all columns.
         for (let i = 0; i < maxPins; i++) {
             // Set all column pins low except for current column.
